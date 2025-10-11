9e53b88d63eced56197e68ba3f95c19a915676de
Aldwin Susantio <aldald.ald197@gmail.com>
2025-10-11 14:04:16 +0700

    Polish UI/UX untuk melihat biaya per tahun asuransi

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,33 +4,40 @@ import { Phone, Mail } from "lucide-react";
 export function Header() {
   return (
     <header className="bg-white shadow-sm fixed w-full top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-2">
-          <div className="flex flex-col items-center">
-            <h1 className="text-primary">Ivan Raharja</h1>
-            <span className="text-muted-foreground">MSIG Insurance Partner</span>
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center ">
+          <div className="flex gap-4">
+            <img src="/ivan-profile.png" className="w-12 m-2 rounded-lg"></img>
+            <div className="flex flex-col justify-center">
+              <h1 className="text-primary">Ivan Raharja</h1>
+              <span className="text-muted-foreground">
+                MSIG Insurance Partner
+              </span>
+            </div>
           </div>
           <nav className="hidden md:flex items-center space-x-8">
-            <a href="#home" className="text-foreground hover:text-primary transition-colors">Home</a>
-            <a href="#about" className="text-foreground hover:text-primary transition-colors">About</a>
-            <a href="#services" className="text-foreground hover:text-primary transition-colors">Services</a>
-          {/*  <a href="#testimonials" className="text-foreground hover:text-primary transition-colors">Testimonials</a>
-            <a href="#contact" className="text-foreground hover:text-primary transition-colors">Contact</a> */}
+            {/* <a href="#home" className="text-foreground hover:text-primary transition-colors">Home</a>
+                    <a href="#about" className="text-foreground hover:text-primary transition-colors">About</a>
+                    <a href="#services" className="text-foreground hover:text-primary transition-colors">Services</a>
+                  <a href="#testimonials" className="text-foreground hover:text-primary transition-colors">Testimonials</a>
+                    <a href="#contact" className="text-foreground hover:text-primary transition-colors">Contact</a> */}
           </nav>
-          
+
           <div className="flex items-center space-x-4">
             <div className="hidden lg:flex items-center space-x-4 text-sm text-muted-foreground">
               <div className="flex items-center">
                 <Phone className="w-4 h-4 mr-2" />
-                <span>0877-8055-7401</span>
+                <span className="text-lg">0877-8055-7401</span>
               </div>
               <div className="flex items-center">
-                <Mail className="w-4 h-4 mr-2" />
-                <span>ivanraha1028@gmail.com</span>
+                <Mail className="w-4 h-4 mr-2 " />
+                <span className="text-lg">ivanraha1028@gmail.com</span>
               </div>
             </div>
             <Button size="lg" className="px-3" asChild>
-              <a href="https://wa.me/6287780557401?text=Halo!%20Saya%20tertarik%20untuk%20konsultasi%20gratis%20asuransi.">Konsultasi Gratis</a>
+              <a href="https://wa.me/6287780557401?text=Halo!%20Saya%20tertarik%20untuk%20konsultasi%20gratis%20asuransi.">
+                Konsultasi Gratis
+              </a>
             </Button>
           </div>
         </div>
--- src/components/Pricelist.tsx
+++ src/components/Pricelist.tsx
@@ -6,7 +6,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
 import {
   Select,
@@ -14,105 +14,136 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 
-import { useState, useEffect } from "react";  
-import { Slider } from "@/components/ui/slider"
-import { Label } from "@/components/ui/label"
+import { useState, useEffect } from "react";
+import { Slider } from "@/components/ui/slider";
+import { Label } from "@/components/ui/label";
 import * as XLSX from "xlsx";
 
-
 export function Pricelist() {
+  const [data, setData] = useState<any[] | null>(null);
+  const [age, setAge] = useState<number>(25); //initial age
+  const [gender, setGender] = useState<string>("");
 
-    const [data, setData] = useState<any[]|null>(null);
-    const [age, setAge] = useState<number>(25); //initial age
-    const [gender, setGender] = useState<string>("")
-    
+  type Row = [...any[]]; // first col is the age range like "12-45"
 
-    type Row = [...any[]]; // first col is the age range like "12-45"
+  useEffect(() => {
+    setTimeout(() => {
+      setData(null);
+    }, 300); //set data to null first to show loading state if user change
 
-    useEffect(() => {
-        
-        setTimeout(() => {
-            setData(null)
-        }, 300); //set data to null first to show loading state if user change
+    const fetchData = async () => {
+      const res = await fetch(encodeURI("/Tabel Premi.xlsx"));
+      const buf = await res.arrayBuffer();
+      const wb = XLSX.read(buf, { type: "array" });
+      const ws = wb.Sheets[gender];
+      const allData = XLSX.utils.sheet_to_json<Row>(ws, {
+        header: 1,
+        defval: "",
+      }); //defval ="" to avoid 'undefined' cells when empty
+      const filteredByGender = allData.slice(2);
+      const firstRegex = /^\d+/;
+      const lastRegex = /\d+$/;
+      setData(
+        filteredByGender.filter(
+          (
+            row //filter rows based on age range
+          ) =>
+            Number(row[0]?.match(firstRegex)) <= age &&
+            Number(row[0].match(lastRegex)) >= age
+        )
+      );
+    };
 
-        const fetchData = async () => {
-            
-            const res = await fetch(encodeURI("/Tabel Premi.xlsx"))
-            const buf = await res.arrayBuffer();
-            const wb = XLSX.read(buf, { type: "array" });
-            const ws = wb.Sheets[gender];
-            const allData = XLSX.utils.sheet_to_json<Row>(ws, { header: 1, defval: "" }); //defval ="" to avoid 'undefined' cells when empty
-            const filteredByGender = allData.slice(2)
-            const firstRegex = /^\d+/
-            const lastRegex = /\d+$/
-            setData(filteredByGender.filter(row => //filter rows based on age range
-                Number(row[0]?.match(firstRegex)) <= age &&
-                Number(row[0].match(lastRegex)) >= age)
-            ); 
-        }
+    const timer = setTimeout(() => {
+      fetchData();
+    }, 500);
 
-        const timer = setTimeout(() => {
-            fetchData()
-        }, 500);
+    return () => clearTimeout(timer); //cleanup function to clear timer on unmount
+  }, [age, gender]);
 
-        return () => clearTimeout(timer); //cleanup function to clear timer on unmount
-    }, [age, gender])
+  return (
+    <section
+      id="pricelist"
+      className="relative pt-8 w-full gap-y-8 min-h-screen flex flex-col items-center bg-primary/7"
+    >
+      <Label>Pilih Gender:</Label>
+      <Select onValueChange={(val) => setGender(val)}>
+        {" "}
+        {/* val si always a string that dont return value*/}
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Gender" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="male">Laki-laki</SelectItem>
+          <SelectItem value="female">Perempuan</SelectItem>
+        </SelectContent>
+      </Select>
+      {gender && (
+        <div className="gap-y-6 flex flex-col items-center w-full">
+          <Label>Sesuaikan Usia Anda:</Label>
+          <Slider
+            defaultValue={[age]}
+            max={70}
+            step={1}
+            onValueChange={(v) => setAge(v[0])}
+            className="w-1/4"
+          />
+          <span>{age}</span>
 
-
-    return (
-        <section id="pricelist" className="relative pt-8 w-full gap-y-8 min-h-screen flex flex-col items-center bg-primary/7">
-            <Label>Pilih Gender:</Label>
-            <Select onValueChange={(val) => setGender(val)}> {/* val si always a string that dont return value*/}
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Gender" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value="male">Laki-laki</SelectItem>
-                    <SelectItem value="female">Perempuan</SelectItem>
-                </SelectContent>
-            </Select>
-            <Label>Sesuaikan Usia Anda:</Label>
-            <Slider defaultValue={[age]} max={70} step={1} onValueChange={(v) => setAge(v[0])} className="w-1/4"/>
-            <span>{age}</span>
-            <Table className="w-4/5 border-separate border-4 text-center mx-auto">
-                <TableCaption>Tabel Premi Asuransi Kesehatan</TableCaption>
-                <TableHeader className="bg-blue-200">
-                    <TableRow className="hover:bg-transparent">
-                        <TableHead className="text-center" rowSpan={2}>Usia (tahun)</TableHead>
-                        <TableHead className="text-center" rowSpan={2}>Harga/tahun</TableHead>
-                        <TableHead className="text-center" colSpan={3}>Wilayah Cover</TableHead>
-                        <TableHead className="text-center" colSpan={2}>Tipe Kamar</TableHead>
-                        <TableHead className="text-center" rowSpan={2}>Limit per tahun</TableHead>
-                        <TableHead className="text-center" rowSpan={2}>Booster</TableHead>
-                    </TableRow>
-                    <TableRow className="hover:bg-transparent">
-                        <TableHead className="text-center">Indonesia</TableHead>
-                        <TableHead className="text-center">Asia (kecuali SG, HK, JP)</TableHead>
-                        <TableHead className="text-center">Asia</TableHead>
-                        <TableHead className="text-center">1 Tempat Tidur</TableHead>
-                        <TableHead className="text-center">2 Tempat Tidur</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {(data ?? []).map(el => (
-                        <TableRow className="odd:bg-white even:bg-gray-100" key={el[0]}>
-                            <TableCell>{el[0]}</TableCell>
-                            <TableCell>{el[1].toLocaleString("id-ID")}</TableCell>
-                            <TableCell>{el[2]}</TableCell>  
-                            <TableCell>{el[3]}</TableCell>
-                            <TableCell>{el[4]}</TableCell>
-                            <TableCell>{el[5]}</TableCell>
-                            <TableCell>{el[6]}</TableCell>
-                            <TableCell>{el[7]}</TableCell>
-                            <TableCell>{el[8]}</TableCell>                  
-                        </TableRow>
-                    ))
-                    }
-                </TableBody>
-            </Table>
-        </section>
-    )   
+          <Table className="w-4/5 border-separate border-4 text-center mx-auto">
+            <TableCaption>Tabel Premi Asuransi Kesehatan</TableCaption>
+            <TableHeader className="bg-blue-200">
+              <TableRow className="hover:bg-transparent">
+                <TableHead className="text-center" rowSpan={2}>
+                  Usia (tahun)
+                </TableHead>
+                <TableHead className="text-center" rowSpan={2}>
+                  Harga/tahun
+                </TableHead>
+                <TableHead className="text-center" colSpan={3}>
+                  Wilayah Cover
+                </TableHead>
+                <TableHead className="text-center" colSpan={2}>
+                  Tipe Kamar
+                </TableHead>
+                <TableHead className="text-center" rowSpan={2}>
+                  Limit per tahun
+                </TableHead>
+                <TableHead className="text-center" rowSpan={2}>
+                  Booster
+                </TableHead>
+              </TableRow>
+              <TableRow className="hover:bg-transparent">
+                <TableHead className="text-center">Indonesia</TableHead>
+                <TableHead className="text-center">
+                  Asia (kecuali SG, HK, JP)
+                </TableHead>
+                <TableHead className="text-center">Asia</TableHead>
+                <TableHead className="text-center">1 Tempat Tidur</TableHead>
+                <TableHead className="text-center">2 Tempat Tidur</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {(data ?? []).map((el) => (
+                <TableRow className="odd:bg-white even:bg-gray-100" key={el[0]}>
+                  <TableCell>{el[0]}</TableCell>
+                  <TableCell>{el[1].toLocaleString("id-ID")}</TableCell>
+                  <TableCell>{el[2]}</TableCell>
+                  <TableCell>{el[3]}</TableCell>
+                  <TableCell>{el[4]}</TableCell>
+                  <TableCell>{el[5]}</TableCell>
+                  <TableCell>{el[6]}</TableCell>
+                  <TableCell>{el[7]}</TableCell>
+                  <TableCell>{el[8]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      )}
+    </section>
+  );
 }
 //initialValue is starting value of stateVariable (only run on 1st render)
